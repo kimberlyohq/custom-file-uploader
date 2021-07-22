59b85eb389c987e38dbd7c22f6c76279f3162b7d
kimberly ong <e0424807@u.nus.edu>
2021-07-22 16:08:54 +0800

    refactor: use useRef to store the value of progress

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 
 import "./App.css";
 
 import { customFetch } from "./customFetch";
 
 const URL = "http://localhost:8000/files";
+//let progress = 0;
 
 function App() {
   const inputRef = useRef();
 
+  // set initial value to 0
+  let progress = useRef();
+
   const [isPaused, setIsPaused] = useState(false);
 
-  let progress = 0;
-
   let xmlRequest = [];
-
-  useEffect(() => {
-    console.log(progress);
-  })
-
-  useEffect(() => {
-    
-    console.log(isPaused);
-  }, [isPaused]);
 
   const onUploadProgress = (event) => {
     console.log(`${event.loaded} / ${event.total}`);
@@ -37,14 +30,14 @@ function App() {
 
   // Implement partial chunk request (When the file size is not a multiple of chunk size)
   const createPartialFileChunk = (file, chunkStart) => {
-    const fileChunk = file.slice(progress, chunkStart, file.size);
+    const fileChunk = file.slice(progress.current, chunkStart, file.size);
     return fileChunk;
   };
 
   // Create a custom request
   const createCustomRequest = (blob: Blob) => {
     const formData = new FormData();
-    formData.append(`Blob ${progress} ${progress + blob.size}`, blob);
+    formData.append(`Blob ${progress.current} ${progress.current + blob.size}`, blob);
 
     const customRequest = {
       url: `${URL}/upload`,
@@ -56,67 +49,87 @@ function App() {
     return customRequest;
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
+  const handleUpload = async () => {
     const file = inputRef.current.files[0];
+
+    if (!file || file.size === 0) {
+      alert("No file uploaded");
+      return;
+    }
 
     const CHUNK_SIZE = 10 * 1024;
 
-    while (progress + CHUNK_SIZE < file.size) {
-      const fileChunk = createFixedFileChunk(file, progress, CHUNK_SIZE);
+    if(progress.current === undefined) {
+      progress.current = 0;
+    }
+
+    console.log("START" + progress.current);
+
+    while (progress.current + CHUNK_SIZE < file.size) {
+      const fileChunk = createFixedFileChunk(file, progress.current, CHUNK_SIZE);
       const customRequest = createCustomRequest(fileChunk);
       try {
         const response = await customFetch(customRequest);
         const message = (await response.json()).message;
-        console.log(`${message} ${progress} - ${progress + CHUNK_SIZE}`);
-        progress += CHUNK_SIZE;
+        console.log(`${message} ${progress.current} - ${progress.current + CHUNK_SIZE}`);
+        progress.current += CHUNK_SIZE;
       } catch (err) {
         console.log(err);
       }
     }
 
-    if(progress === file.size) {
-      return ;
+    if (progress.current === file.size) {
+      return;
     }
 
-    if (progress !== file.size) {
-      const partialFileChunk = createPartialFileChunk(file, progress);
+    if (progress.current !== file.size) {
+      const partialFileChunk = createPartialFileChunk(file, progress.current);
       const customRequest = createCustomRequest(partialFileChunk);
       try {
         const response = await customFetch(customRequest);
         const message = (await response.json()).message;
-        console.log(`${message} ${progress} - ${file.size}`);
-        progress = file.size;
+        console.log(`${message} ${progress.current} - ${file.size}`);
+        progress.current = file.size;
       } catch (err) {
         console.log(err);
       }
     }
 
-    console.log(progress === file.size);
-    // reset the progress
-    progress  = 0;
+    if (progress.current === file.size) {
+      console.log("COMPLETED");
+    }
 
+    // reset the progress
+    progress.current = 0;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await handleUpload();
   };
 
   const handlePause = (event) => {
     event.preventDefault();
-    setIsPaused(isPaused);
+    setIsPaused(true);
+    xmlRequest.find((xhr) => xhr.abort());
+    xmlRequest = [];
   };
 
   const handleCancel = (event) => {
     event.preventDefault();
+    xmlRequest.find((xhr) => xhr.abort());
 
     // reset the request list
     xmlRequest = [];
 
     // reset progress
-    progress = 0;
+    progress.current = 0;
   };
 
-  const handleResume = (event) => {
+  const handleResume = async (event) => {
     event.preventDefault();
     setIsPaused(false);
+    await handleUpload();
   };
 
   return (
