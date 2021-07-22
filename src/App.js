f32192ae7c61cba8e1cbfa116683d5fb25e8f96e
kimberly ong <e0424807@u.nus.edu>
2021-07-22 09:42:03 +0800

    feat: implement logic to break file into fixed size chunks

--- src/App.js
+++ src/App.js
@@ -12,20 +12,52 @@ function App() {
   const handleFileUpload = (event) => {
     const files = event.target.files;
     setFile(files[0]);
-  }
+  };
 
-  const handleSubmit = () => {
+  const uploadFile = async () => {
+    const customRequest = {
+      url: `${URL}/upload`,
+      method: "POST",
+    };
 
-  }
+    try {
+      const response = await customFetch(customRequest);
+      console.log(await response.json());
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  // Break the file into fixed size Blobs
+  const createFileChunks = (file: File) => {
+    const fileChunkList = [];
+    const fileSize = file.size;
+
+    let current = 0;
+    const STEP = 10 * 1024;
+
+    while (current < fileSize) {
+      //var newBlob = blob.slice(start, end, contentType);
+      const fileChunk = file.slice(current, current + STEP);
+      current += STEP;
+      fileChunkList.push({ file: fileChunk });
+    }
+
+    fileChunkList.push({file: file.slice(current, fileSize)});
+  
+    return fileChunkList;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const fileChunkList = createFileChunks(file);
+    uploadFile();
+  };
 
   return (
     <div className="App">
       <form>
-        <input
-          type="file"
-          onChange={handleFileUpload}
-        />
+        <input type="file" onChange={handleFileUpload} />
         <button type="submit" onClick={handleSubmit}>
           Upload
         </button>
